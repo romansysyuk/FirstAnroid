d3c2fc0da131e4f4b0ee447a3b8ca3b722cd17f4
romansys <kobebryant41@gmail.com>
2016-11-09 00:45:04 +0100

    Test

---
+++ html/src/rzeszow/pl/drop/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package rzeszow.pl.drop.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import rzeszow.pl.drop.Drop;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(800,480);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Drop();
+        }
+}
